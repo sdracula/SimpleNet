6a505d691bed6352806104a9f936429a9172a604
sdracula <50819088+sdracula@users.noreply.github.com>
2024-01-08 13:46:21 +0800

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -88,7 +88,7 @@ from skimage import measure
 def compute_pro(masks, amaps, num_th=200):
 
     df = pd.DataFrame([], columns=["pro", "fpr", "threshold"])
-    binary_amaps = np.zeros_like(amaps, dtype=np.bool)
+    binary_amaps = np.zeros_like(amaps, dtype=bool)
 
     min_th = amaps.min()
     max_th = amaps.max()
@@ -112,7 +112,7 @@ def compute_pro(masks, amaps, num_th=200):
         fp_pixels = np.logical_and(inverse_masks, binary_amaps).sum()
         fpr = fp_pixels / inverse_masks.sum()
 
-        df = df.append({"pro": np.mean(pros), "fpr": fpr, "threshold": th}, ignore_index=True)
+        df = df._append({"pro": np.mean(pros), "fpr": fpr, "threshold": th}, ignore_index=True)
 
     # Normalize FPR from 0 ~ 1 to 0 ~ 0.3
     df = df[df["fpr"] < 0.3]
